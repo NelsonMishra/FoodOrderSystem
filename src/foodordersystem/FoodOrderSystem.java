a987b932215d35bda3c4b1bc57bf8ff65d805ab2
joker <nelsonmishra007@gmail.com>
2023-04-01 21:02:11 +0530

    first commit

---
+++ src/foodordersystem/FoodOrderSystem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package foodordersystem;
+
+/**
+ *
+ * @author user
+ */
+public class FoodOrderSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new Login().setVisible(true); //display the logn form at the first
+    }
+    
+}
